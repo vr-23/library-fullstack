0a5838fad69af3fb46793ec19e6c9472c95394e0
KalpanaMannem <mannem.kalpana13@gmail.com>
2024-07-27 16:17:55 -0500

    Added Login,Signup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login';
+
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Signup from './Components/Signup';
+// import SignUp from './Components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Library
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/SignIn" element={<Login />} />
+        </Routes>
+
+      </BrowserRouter>
+      {/* <Login />
+      <SignUp /> */}
+    </>
   );
 }
 
