5b2d53cbc96fc4e0b249e6269d6ad970195d7a35
vr-23 <vishnuvardhanreddy.sudireddy@gmail.com>
2024-07-27 13:05:20 -0500

    resolving conflicts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Library
+        </a>
+      </header>
     </div>
   );
 }
