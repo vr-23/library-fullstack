56a72d66c9a400c2955bb4ea4ff5ce0daabe6ea1
KalpanaMannem <mannem.kalpana13@gmail.com>
2024-07-28 11:26:08 -0500

    updated dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,23 +1,52 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import { booksData } from '../Data/Books'
+import './Dashboard.css';
 
 const Dashboard = () => {
+    const data = booksData
     return ( 
         <div>
             <Navbar />
             <p>Dashboard</p>
-            <div>
+            <div class="container-fluid">
+                <div class="row">
+            {data.map((item)=>{
+            return (
+                
+             <div class="col-sm-2 d-flex justify-content-center">
                 <div class="card" style={{width: '12rem', border:'1px solid black'}}>
-                    <img class="card-img-top" src="..." alt="Card image cap" />
-                        <div class="card-body align">
+                    <img className="img" src={item.image} alt="image not found" width="180px"></img>
+                    <div class="card-body align">
+                            <h5 class="card-title">{item.Author}</h5>
+                            <p class="card-text">{item.price}</p>
+                            <a href="#" class="btn btn-primary">checkout</a>
+                        </div>
+                        </div>
+                       
+                </div>
+            )
+        })}
+            {/* <div>
+                <div class="card" style={{width: '12rem', border:'1px solid black'}}>
+                <div class="card-body align">
                             <h5 class="card-title">Book Name</h5>
                             <p class="card-text">Price</p>
                             <a href="#" class="btn btn-primary">checkout</a>
                         </div>
+                   
                 </div>
-            </div>
+            </div> */}
+        </div>
+        </div>
         </div>
     )
 }
 
+{/* <img class="card-img-top" src="..." alt="Card image cap" />
+                        <div class="card-body align">
+                            <h5 class="card-title">Book Name</h5>
+                            <p class="card-text">Price</p>
+                            <a href="#" class="btn btn-primary">checkout</a>
+                        </div> */}
 export default Dashboard
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App';
+// import App from './components/App/App';
+import App from './Components/App/App'
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
